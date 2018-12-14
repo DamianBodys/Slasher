7c7f2dcb13abcf0b7594f32d4e7e5a9bf012975f
Damian Bodys <damian.bodys.94@gmail.com>
2018-12-14 21:38:40 +0100

    Initial commit

---
+++ Assets/Cameras/Code/Editor/TopDown_Camera_Editor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Slasher.Cameras
+{
+    public class TopDown_Camera_Editor : MonoBehaviour
+    {
+        #region Variables
+        #endregion
+
+        #region Main Methods
+        #endregion
+    }
+}
---
+++ Assets/LookAtCoursor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCoursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    
+
+    // Update is called once per frame
+    void Update () {
+        //Mouse Position in the world. It's important to give it some distance from the camera. 
+        //If the screen point is calculated right from the exact position of the camera, then it will
+        //just return the exact same position as the camera, which is no good.
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10f);
+        //Angle between mouse and this object
+        float angle = AngleBetweenPoints(transform.position, mouseWorldPosition);
+    
+        //Ta daa
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+
+    float AngleBetweenPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+
+}
+
