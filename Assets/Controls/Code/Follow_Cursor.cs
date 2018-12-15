0672391665cb70735be7c63dfdfe1dddd82ef803
Damian Bodys <damian.bodys.94@gmail.com>
2018-12-15 18:38:04 +0100

    Player now moves in world space

---
+++ Assets/Controls/Code/Follow_Cursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow_Cursor : MonoBehaviour {
+
+    // Update is called once per frame
+    void Update () {
+
+        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+        }
+        
+
+        //Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+        //Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        //lookPos = lookPos - transform.position;
+        //float angle = Mathf.Atan2(lookPos.z, lookPos.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.AngleAxis(-angle, Vector3.up);
+    }
+    
+}
